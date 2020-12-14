75cfb1164eb15ed9b8eac462dee72f2ad1dc6d78
Nelson <nescalan@gmail.com>
2020-12-14 14:42:37 -0600

    3: Archivo de Rutas

---
+++ src/routes/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "../routes/Home";
+
+const App = () => (
+  <BrowserRouter>
+    <Route exact path="/" component="Home" />
+  </BrowserRouter>
+);
+
+export default App;
