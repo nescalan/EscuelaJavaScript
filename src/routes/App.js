6234128bd23dc1668fca7eb427ec8c26698b87a0
Nelson <nescalan@gmail.com>
2020-12-14 16:47:20 -0600

    3: Creando Archivo de Rutas - Inicial

--- src/routes/App.js
+++ src/routes/App.js
@@ -5,8 +5,8 @@ import Login from "../containers/Login";
 
 const App = () => (
   <BrowserRouter>
-    {/* <Route exact path="/" component="Home" /> */}
-    {/* <Route exact path="/Login" component="Login" /> */}
+    <Route exact path="/" component="Home" />
+    <Route exact path="/Login" component="Login" />
   </BrowserRouter>
 );
 
