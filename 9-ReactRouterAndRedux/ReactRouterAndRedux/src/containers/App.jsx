8163cad12594ba2b1574ddbfb03828ac60d1dbf7
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2020-12-14 20:50:14 -0600

    Second Commit With No New Info

---
+++ 9-ReactRouterAndRedux/ReactRouterAndRedux/src/containers/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+import Search from "../components/Search";
+import Categories from "../components/Categories";
+import Carousel from "../components/Carousel";
+import CarouselItem from "../components/CarouselItem";
+import Footer from "../components/Footer";
+import useInitialState from "../hooks/useInitialState";
+import "../assets/styles/App.scss";
+
+const App = () => {
+  //   const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState({
+    mylist: [],
+    trends: [],
+    originals: [],
+  });
+
+  useEffect(() => {
+    fetch("http://localhost:3000/initalState")
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
+
+  //   console.log(videos);
+
+  return (
+    <div className="App">
+      <Header />
+      <Search />
+
+      {videos.mylist.lenth > 0 && (
+        <Categories title="Mi Lista">
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
+
+      <Categories title="Tendencias">
+        <Carousel>
+          {videos.trends.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+
+      <Categories title="Originales de Platzi Video">
+        <Carousel>
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
